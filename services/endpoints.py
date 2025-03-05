ad7c4eb8a7761cb3d0616dc9a550ad6200cb2f01
Laptev Roman <romashlapt@mail.ru>
2025-03-05 14:17:02 +0700

    update

--- services/endpoints.py
+++ services/endpoints.py
@@ -12,25 +12,24 @@ router = APIRouter()
 
 @router.post("/sign_up")
 async def sign_up(user: SignUp, session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Users).where(Users.email == user.email))
-    res = result.scalar_one_or_none()
-    if not res:
-        hashed_password = pwd_context.hash(user.password)
-        new_user = Users(email=user.email, password=hashed_password)
-        session.add(new_user)
-        await session.commit()
-        return {"message": "Успешная регистрация!"}
-    else:
+    query = await session.execute(select(Users).where(Users.email == user.email))
+    result = query.scalar_one_or_none()
+    if result:
         raise CustomException(detail="Пользователь с таким именем уже существует", status_code=404)
+    hashed_password = pwd_context.hash(user.password)
+    new_user = Users(email=user.email, password=hashed_password)
+    session.add(new_user)
+    await session.commit()
+    return {"message": "Успешная регистрация!"}
 
 
 @router.post("/login")
 async def login(user: Login, session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Users).where(Users.email == user.email))
-    res = result.scalar_one_or_none()
-    if not res:
+    query = await session.execute(select(Users).where(Users.email == user.email))
+    result = query.scalar_one_or_none()
+    if not result:
         raise CustomException(detail="Пользователь с таким именем не найден", status_code=404)
-    if not pwd_context.verify(user.password, res.password):
+    if not pwd_context.verify(user.password, result.password):
         raise CustomException(detail="Неверное имя или пароль", status_code=404)
     token = make_jwt_token(user.email)
     return {"access_token": token, "token_type": "bearer"}
@@ -44,3 +43,24 @@ async def create_referral_code(user: Users = Depends(get_current_user),
     await session.commit()
     await session.refresh(user)
     return {"message": f"Реферальный код для {user.email} - {referral_code}"}
+
+
+@router.get("/delete_code")
+async def delete_referral_code(user: Users = Depends(get_current_user),
+                               session: AsyncSession = Depends(get_session)):
+    if not user.referral_code:
+        raise CustomException(detail="У вас нет реферального кода", status_code=404)
+    user.referral_code = None
+    await session.commit()
+    return {"message": "Реферальный код успешно удален"}
+
+
+@router.get("/create_code/{email}")
+async def get_referral_code(email: str, session: AsyncSession = Depends(get_session)):
+    query = await session.execute(select(Users).where(Users.email == email))
+    user = query.scalar_one_or_none()
+    if not user:
+        raise CustomException(detail="Пользователь не найден", status_code=404)
+    if user.referral_code is None:
+        raise CustomException(detail="Реферальный код не найден", status_code=404)
+    return {"message": f"Реферальный код данного пользователя - {user.referral_code}"}
--- services/security.py
+++ services/security.py
@@ -33,8 +33,8 @@ def get_user_from_jwt_token(jwt_token: str):
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
                            session: AsyncSession = Depends(get_session)):
     email = get_user_from_jwt_token(token)
-    result = await session.execute(select(Users).where(Users.email == email))
-    user = result.scalar_one_or_none()
+    query = await session.execute(select(Users).where(Users.email == email))
+    user = query.scalar_one_or_none()
     if not user:
         raise CustomException(detail="Пользователь с таким именем не найден", status_code=404)
     return user
